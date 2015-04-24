f208fcb121fea1451f259446f2d84a9ade8b3c16
idess <m.idess@gmail.com>
2015-04-24 09:43:01 +0900

    menu 데이터 키워드 변경

--- src/main/webapp/js/app/views/common/menu.js
+++ src/main/webapp/js/app/views/common/menu.js
@@ -8,18 +8,27 @@ define(function(require) {
 	// require library
 	var $ = require('jquery'),
 		_ = require('underscore'),
-		Backbone = require('backbone');
+		Backbone = require('backbone'),
+		TbAttackInfo = require('views/popup/attackInfoPopup'),
+		TbIpRetrieve = require('views/tools/ipRetrievePopup'),
+		TabView = require('views/popup/tab');
+
+	var toolbarTpl = require('text!tpl/common/toolbar.html'),
+		toolbarTemplate = _.template(toolbarTpl);
 
 	// require i18n
-	var locale = require('i18n!nls/menu');
+	var locale = require('i18n!nls/menu'),
+		strLocale = require('i18n!nls/str');
 
 	var MenuView = Backbone.View.extend({
 		tagName: "ul",
-		className: "nav navbar-nav",
-		parentMenuTemplate: _.template('<li class="<%= menu.menukey %>-menu"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><%= menuName %><span class="caret"></span></a><ul class="dropdown-menu" role="menu"></ul></li>'),
-		childMenuTemplate: _.template('<li><a href="#<%= menu.url %>"><%= menuName %></a></li>'),
+		className: "nav",
+		parentMenuTemplate: _.template('<li class="has-sub <%= menu.menuKey %>"><a class="menu-icon"><b class="caret pull-right"></b> <i class="fa fa-laptop"></i> <span><%= menuName %></span></a><ul class="sub-menu"></ul></li>'),
+		childMenuTemplate: _.template('<li class="<%= menu.url %>"><a href="#<%= menu.url %>"><%= menuName %></a></li>'),
 		initialize: function(options) {
 			this.render();
+		},
+		events: {
 		},
 		render: function() {
 			var view = this;
@@ -27,15 +36,15 @@ define(function(require) {
 			this.$el.empty();
 
 			_.each(this.collection, function(menu) {
-				if (menu.higherkey) {
-					$('.' + menu.higherkey + '-menu ul', view.el).append(view.childMenuTemplate({
+				if (menu.upperMenuKey) {
+					$('.' + menu.upperMenuKey + ' ul', view.el).append(view.childMenuTemplate({
 						menu: menu,
-						menuName: eval('locale.menu.' + menu.menukey)
+						menuName: eval('locale.menu.' + menu.menuKey)
 					}));
 				} else {
 					view.$el.append(view.parentMenuTemplate({
 						menu: menu,
-						menuName: eval('locale.menu.' + menu.menukey)
+						menuName: eval('locale.menu.' + menu.menuKey)
 					}));
 				}
 			});
